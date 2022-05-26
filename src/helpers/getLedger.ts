b84e2013549307b745bac122ec66adebbcc82810
Michel Erokhov <mixaileroxov@gmail.com>
2022-05-26 21:49:53 +0300

    rename getAddressToMerkle

--- src/helpers/getLedger.ts
+++ src/helpers/getLedger.ts
@@ -1,10 +1,10 @@
 import { SealCredLedger } from '@big-whale-labs/seal-cred-ledger-contract'
 import Ledger from 'models/Ledger'
-import getAddressToMerkle from 'helpers/getAddressToMerkle'
+import getAddressToMerkleRoot from 'helpers/getAddressToMerkleRoot'
 import getLedgerRecord from 'helpers/getLedgerRecord'
 
 export default async function (sealCredLedger: SealCredLedger) {
-  const addressToMerkle = await getAddressToMerkle(sealCredLedger)
+  const addressToMerkle = await getAddressToMerkleRoot(sealCredLedger)
 
   const ledger = {} as Ledger
 
